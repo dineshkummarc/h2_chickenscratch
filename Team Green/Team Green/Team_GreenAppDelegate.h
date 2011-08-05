5ba59354c412b53efbb216b113f053e4fc9fdf13
Gareth Walters <g@rethwalte.rs>
2011-08-05 11:03:10 -0700

    Fixed name

---
+++ Team Green/Team Green/Team_GreenAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Team_GreenAppDelegate.h
+//  Team Green
+//
+//  Created by Gareth Walters on 8/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Team_GreenViewController;
+
+@interface Team_GreenAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) Team_GreenViewController *viewController;
+
+@end
---
+++ Team Green/Team Green/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Team Green
+//
+//  Created by Gareth Walters on 8/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Team_GreenAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Team_GreenAppDelegate class]));
+    }
+}
