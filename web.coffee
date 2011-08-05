dc3b2e008772152ae6ad9f3cf36d686e1f03b636
Rodney K <me@rodneykoch.com>
2011-08-05 14:33:19 -0700

    .

--- web.coffee
+++ web.coffee
@@ -1,13 +1,20 @@
 
 
-express = require('express')
-
+express = require 'express'
 app = express.createServer(express.logger())
+app.use express.bodyParser()
+app.use express.cookieParser()
+app.use express.methodOverride()
+app.use app.router
+app.register '.coffee', require('ejs')
+app.set 'views', process.cwd() + '/app/views'
+app.set 'view engine', 'ejs'
+app.use express.static(process.cwd() + '/public')
+app.use express.static(process.cwd() + '/public/css')
+app.use express.static(process.cwd() + '/public/js')
+app.use(express.errorHandler { dumpExceptions: true, showStack: true })
 
-app.get '/', (request, response) =>
-  response.send('Hello World!')
-
-port = process.env.PORT || 3000
-app.listen port, () =>
-  console.log("Listening on " + port)
+app.get '/', (req, res, next) -> res.render 'index'
+app.listen 3000
+console.log("Listening on " + port)
 
--- web.js
+++ web.js
@@ -1,13 +1,24 @@
 (function() {
-  var app, express, port;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var app, express;
   express = require('express');
   app = express.createServer(express.logger());
-  app.get('/', __bind(function(request, response) {
-    return response.send('Hello World!');
-  }, this));
-  port = process.env.PORT || 3000;
-  app.listen(port, __bind(function() {
-    return console.log("Listening on " + port);
-  }, this));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.register('.coffee', require('ejs'));
+  app.set('views', process.cwd() + '/app/views');
+  app.set('view engine', 'ejs');
+  app.use(express.static(process.cwd() + '/public'));
+  app.use(express.static(process.cwd() + '/public/css'));
+  app.use(express.static(process.cwd() + '/public/js'));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  app.get('/', function(req, res, next) {
+    return res.render('index');
+  });
+  app.listen(3000);
+  console.log("Listening on " + port);
 }).call(this);
