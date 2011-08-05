51ac8219bb02a7c8719d3e2685272a214a80be67
Rodney K <me@rodneykoch.com>
2011-08-05 12:32:48 -0700

    touch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iscrip::Application.config.secret_token = '3094b9d7a357b042a42c62435bf6ab9ed688f76e553c0a8cb21ad49d16933e0be61fabf2b6dd628449191a1ff3b6c09b8667b81d82ea4a6cb3c0bb77893e10b9'
