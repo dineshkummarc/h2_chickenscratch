15eff93abe51d92ac1316005c0274fabd2ee58b2
Gareth Walters <g@rethwalte.rs>
2011-08-05 10:56:49 -0700

    BAM

---
+++ PDF Writer/PDF Writer/PDF_WriterAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PDF_WriterAppDelegate.h
+//  PDF Writer
+//
+//  Created by Gareth Walters on 8/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PDF_WriterViewController;
+
+@interface PDF_WriterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) PDF_WriterViewController *viewController;
+
+@end
---
+++ PDF Writer/PDF Writer/PDF_WriterViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDF_WriterViewController.h
+//  PDF Writer
+//
+//  Created by Gareth Walters on 8/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDF_WriterViewController : UIViewController
+
+@end
